4e8c5097e68ec2e036bc4a1891f986a358793032
MarcosCantelli <marcoscantelli@hotmail.com>
2025-11-07 18:14:26 -0300

    Remove default values from vSphere variables in Terraform configuration

--- variables.tf
+++ variables.tf
@@ -1,38 +1,32 @@
 variable "vsphere_user" {
   description = "Usuário para autenticação no vSphere"
   type        = string
-  default     = env("VSPHERE_USER")
 }
 
 variable "vsphere_password" {
   description = "Senha para autenticação no vSphere"
   type        = string
   sensitive   = true
-  default     = env("VSPHERE_PASSWORD")
 }
 
 variable "vsphere_server" {
   description = "Endereço do servidor vSphere"
   type        = string
-  default     = env("VSPHERE_SERVER")
 }
 
 variable "datacenter" {
   description = "Nome do Datacenter"
   type        = string
-  default     = env("VSPHERE_DATACENTER")
 }
 
 variable "cluster" {
   description = "Nome do Cluster"
   type        = string
-  default     = env("VSPHERE_CLUSTER")
 }
 
 variable "network" {
   description = "Nome da Rede"
   type        = string
-  default     = env("VSPHERE_NETWORK")
 }
 
 # Variables that will be prompted
